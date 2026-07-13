a96d01e501a285a663934dd608931cf68622a490
Patrick Roy <patrick24r@gmail.com>
2026-07-13 15:37:10 -0400

    Fix print format for screen

--- rockbottom/printer.cc
+++ rockbottom/printer.cc
@@ -65,7 +65,7 @@ void Printer::PrintTimes(std::vector<BusTime> bus_times) {
 
     for (int row = 0; row < rowsToPrint && row < bus_times.size(); row++) {
       char line_buffer[100];
-      snprintf(line_buffer, sizeof(line_buffer), "%-3s %17s %2d min",
+      snprintf(line_buffer, sizeof(line_buffer), "%-3s %-16s %2d min",
                bus_times[row].route_id.c_str(),
                bus_times[row].destination.c_str(),
                bus_times[row].minutes_to_arrival);
